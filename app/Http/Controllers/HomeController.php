a9e7376ebe575c12ed1e1ebfa9185ccf9614e91a
Jose Luis Aviles <53350412+AvilesCode@users.noreply.github.com>
2023-05-16 00:20:51 -0600

    Menus nuevos
    cramos menus nuevos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,4 +30,14 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function menuA()
+    {
+        return view('home');
+    }
+
+    public function menuB()
+    {
+        return view('home');
+    }
 }
